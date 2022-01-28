3d47c166ad20b5b56428689f3437c110ebf07bc3
11shimazuM <95361235+11shimazuM@users.noreply.github.com>
2022-01-28 12:31:19 +0900

    Add files via upload

---
+++ PHP01/toiawase.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ユーザー登録</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Machi-Kaji</title>
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+  <link rel="stylesheet" type="text/css" href="kanryou.css">
+</head>
+<header id="vw">
+  <a href="index.html">
+   <h1 class="title">Machi-Kaji</h1>
+  </a>
+  <h4 class="subtitle">~ママ同士の家事代行マッチングサイト~</h4>
+  <nav class="nav">
+  <ul class="menu-group">
+  <li class="menu-item"><a href="#">Machi-Kajiとは</a></li>
+  <li class="menu-item"><a href="#">ログイン</a></li>
+  <li class="menu-item"><a href="index.php">利用登録の方はこちら</a></li>
+  <li class="menu-item"><a href="#">お知らせ</a></li>
+  <li class="menu-item"><a href="toiawase.php">お問い合わせ</a></li>
+  </ul>
+  </nav>
+</header>
+<body>
+<!-- <div style = text-align:center; margin-left:auto; margin-right:auto;
+text-align:left;
+> -->
+<script type="text/javascript">
+  var css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = 'touroku.css';
+  css.type = 'text/css';
+  var head = document.getElementsByTagName('head')[0];
+  head.appendChild(css);
+</script>
+<form action="insert.php" method="post" >
+<fieldset>
+  <h3 class="label-title">◯　お問い合わせ　◯</h3><br><br>
+  <div class="item">
+    <label class="label_left" for="name">お名前</label>
+    <input id="name" type="text" name="name" placeholder="お名前" autocomplete="name" required autofocus /><br>
+  </div>
+  <div class="item">
+    <label class="label_left" for="pass2">お電話番号</label>
+    <input type="tel" name="tel" placeholder="例) 09012345678" required autofocus><br>
+  </div>
+  <div class="item">
+    <label class="label_left" for="line">LINE ID</label>
+    <input type="text" name="line" size="30" value="" placeholder="連絡手段がLINE希望の場合" /><br>
+  </div>
+  <div class="item">
+    <label class="label_left" for="mail">mail</label>
+    <input type="email" name="mail" size="30" value="" placeholder="メールアドレス" autocomplete="email" required autofocus/><br>
+  </div>
+    <div class="item">
+    <fieldset>
+       <legend>お問い合わせ内容</legend>
+       <label for="mokuteki[]">ご利用について</label>
+       <input type="checkbox" name="mokuteki[]" value="料理"><br>
+       <label for="mokuteki[]">サポーターについて</label>
+       <input type="checkbox" name="mokuteki[]" value="掃除"><br>
+       <label for="mokuteki[]">その他</label>
+       <input type="checkbox" name="mokuteki[]" size="100"value=""><br>
+    </fieldset>
+  </div>
+  <div class="item">
+    <fieldset>
+       <legend>ご希望の連絡方法</legend>
+       <label for="mokuteki[]">電話</label>
+       <input type="checkbox" name="mokuteki[]" value="料理"><br>
+       <label for="mokuteki[]">LINE</label>
+       <input type="checkbox" name="mokuteki[]" value="掃除"><br>
+       <label for="mokuteki[]">メール</label>
+       <input type="checkbox" name="mokuteki[]" value="お片付け"><br>
+    </fieldset>
+  </div>
+  <div class="item">
+    <fieldset>
+       <legend>お住まい</legend>
+       <label for="mokuteki[]">札幌市</label>
+       <input type="checkbox" name="mokuteki[]" value="料理"><br>
+       <label for="mokuteki[]">札幌市外</label>
+       <input type="checkbox" name="mokuteki[]" value="掃除"><br>
+       <label for="mokuteki[]">その他</label>
+       <input type="text" name="mokuteki[]" value=""><br>
+    </fieldset>
+  </div>
+  <div class="item">
+    <label class="label_left" for="pass">その他お問い合わせ内容</label>
+    <input type="text" name="pass" size="100" placeholder="こちらに内容をご記入ください"><br>
+  </div>
+  <div class="btns">
+      <input type="submit" value="送信"><br>
+      <input type="reset" value="リセット">
+  </div>
+  </fieldset>
+</form>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<!--自作のJS-->  <!--自作のJS-->
+<script>
+  window.addEventListener("beforeunload", function(e) {
+    var confirmationMessage = "入力内容を破棄します。";
+    e.returnValue = confirmationMessage;
+    return confirmationMessage;
+  });
+</script>
+</body>
+</html>
